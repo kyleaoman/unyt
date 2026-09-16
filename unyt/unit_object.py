86d0999e1c83d3bc85a2f038f93265abbf65b825
Kyle Oman <kyle.a.oman@durham.ac.uk>
2026-09-16 16:35:03 +0100

    Clean up 'option A' and add descriptive comment.

--- unyt/unit_object.py
+++ unyt/unit_object.py
@@ -402,6 +402,7 @@ class Unit:
             else:
                 units = self
             if data.dtype.kind not in ("f", "u", "i", "c"):
+                # we don't know how to handle this, but the other argument might
                 return NotImplemented
             if data.shape == ():
                 return _import_cache_singleton.uq(data, units, bypass_validation=True)
@@ -439,21 +440,8 @@ class Unit:
 
                 return unyt_quantity(1.0, self) / u
             else:
-                # ---------------- OPTION A ---------------
-                # if hasattr(u, "__rtruediv__"):
-                #     try:
-                #         ret = u.__rtruediv__(self)
-                #         if ret is not NotImplemented:
-                #             return ret
-                #     except TypeError:
-                #         pass
-                # raise InvalidUnitOperation(
-                #     f"Tried to divide a Unit object by '{u}' (type {type(u)}). "
-                #     "This behavior is undefined."
-                # )
-                # ---------------- OPTION B ---------------
+                # we don't know how to handle this, but the other argument might
                 return NotImplemented
-                # -----------------------------------------
         elif self.dimensions is logarithmic and not u.is_dimensionless:
             raise InvalidUnitOperation(f"Tried to divide '{self}' and '{u}'.")
         elif u.dimensions is logarithmic and not self.is_dimensionless:
